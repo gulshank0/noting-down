5d421cd5073949603b3521e167c29a820f019d6d
Gulshan Kumar <gulshanbahadur002@gmail.com>
2025-04-10 15:47:43 +0530

    Update page.tsx
    updtaed src

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 function homePage() {
   return (
     <div className='bg-red rounded-xl text-slate-700'>Dont't</div>
+    <div className='bg-red text-2xl font-bold'> This is Heading </div>
   )
 }
 
